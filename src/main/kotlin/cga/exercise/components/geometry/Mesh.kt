d7fc1a7aac98b7f3b1e5a5338db39e0bbcd8e6cc
Hilal Karatay (inf861) <hilal.karatay@smail.th-koeln.de>
2021-05-25 11:21:14 +0200

    code structure

--- src/main/kotlin/cga/exercise/components/geometry/Mesh.kt
+++ src/main/kotlin/cga/exercise/components/geometry/Mesh.kt
@@ -22,17 +22,13 @@ class Mesh(vertexdata: FloatArray, indexdata: IntArray, attributes: Array<Vertex
     private var indexcount = 0
 
     init {
-
-        // Aufgabe 1.2.2:
-
-        // 1. Erstellen und Binden von VAO
-        //Wie legt man Daten auf der GPU an? 3 Schritte
-        //1. Eine ID erzeugen
-        //2. Dann ID aktivieren
-        //3. Dann Daten hochladen
-
+        /* Aufgabe 1.2.2
+        1. Erstellen und Binden von VAO  Wie legt man Daten auf der GPU an? 3 Schritte
+        1. Eine ID erzeugen 2. Dann ID aktivieren 3. Dann Daten hochladen
+        glBufferData: Man ladet die Daten in die GPU, GL_STATIC_DRAW: inizialisert die Daten einmal und verändert diese nicht mehr, rendert mehrmal*/
 
         //TODO:  Place your code here. Generate IDs, bind oyur objects and upload your Mesh data.
+        indexcount = indexdata.size
         vao = glGenVertexArrays()
         glBindVertexArray(vao)
 
@@ -40,59 +36,29 @@ class Mesh(vertexdata: FloatArray, indexdata: IntArray, attributes: Array<Vertex
         glBindBuffer(GL_ARRAY_BUFFER, vbo)
         glBufferData(GL_ARRAY_BUFFER, vertexdata, GL_STATIC_DRAW)
 
-        //glBufferData: Man ladet die Daten in die GPU, GL_STATIC_DRAW: inizialisert die Daten einmal und verändert diese nicht mehr, rendert mehrmals
-        //glEnableVertexAttribArray(0)
-        //glVertexAttribPointer(0, 3, GL_FLOAT, false, 24,0)
-
-        indexcount = indexdata.size
-
         ibo = glGenBuffers()
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexdata, GL_STATIC_DRAW)
 
         //4. VertexAttribute aktivieren & definieren
-        for (i in attributes.indices){
-            glVertexAttribPointer(i,attributes[i].n, attributes[i].type, false, attributes[i].stride, attributes[i].offset.toLong() )   // glVertexAttribPointer: Legt fest wie die verschiedenen Vertex Attribute zu definieren sind
-            glEnableVertexAttribArray(i)                                    // glEnableVertexAttribArray:  sagt OpenGL welches VertexAttribut im Moment aktiv ist
+        for (i in attributes.indices) {
+            glVertexAttribPointer(i,
+                attributes[i].n,
+                attributes[i].type,
+                false,
+                attributes[i].stride,
+                attributes[i].offset.toLong())   // glVertexAttribPointer: Legt fest wie die verschiedenen Vertex Attribute zu definieren sind
+            glEnableVertexAttribArray(i)         // glEnableVertexAttribArray:  sagt OpenGL welches VertexAttribut im Moment aktiv ist
         }
-
 
         glBindVertexArray(0)
         glBindBuffer(GL_ARRAY_BUFFER, 0)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
-
-        // todo: place your code here
-        // todo: generate IDs
-        // todo: bind your objects
-        // todo: upload your mesh data
     }
-/*
-        // todo: bind your objects
-        glBindVertexArray(vao)
 
-        glBindBuffer(GL_ARRAY_BUFFER,vbo)
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,ibo)
-
-        // todo: upload your mesh data
-        GL30.glBufferData(GL_ARRAY_BUFFER,vertexdata, GL_STATIC_DRAW)
-        GL30.glBufferData(GL_ELEMENT_ARRAY_BUFFER,indexdata, GL_STATIC_DRAW)
-
-        for (i in attributes){
-            glEnableVertexAttribArray(i)
-            GL30.glVertexAttribPointer(i,attributes[i].n,attributes[i].type,false, attributes[i].stride, attributes[i].offset)
-        }*/
-
-
-
-
-
-    /**
-     * renders the mesh
-     */
+    //renders the mesh
     fun render() {
-        // todo: place your code here
-        // call the rendering method every frame
+        // todo: place your code here call the rendering method every frame
 
         //VAO binden
         glBindVertexArray(vao)
--- src/main/kotlin/cga/exercise/game/Scene.kt
+++ src/main/kotlin/cga/exercise/game/Scene.kt
@@ -20,13 +20,12 @@ class Scene(private val window: GameWindow) {
 
     //scene setup
     init {
-        val res: OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/sphere.obj", false, false)
 
-        //initial opengl state
-        glClearColor(0.6f, 1.0f, 1.0f, 1.0f); GLError.checkThrow()
+        //initial opengl state + Blauer Hintergrund
+        //glClearColor(0.6f, 1.0f, 1.0f, 1.0f); GLError.checkThrow()
 
         //schwarzer Hintergrund
-        //glClearColor (0.0f, 0.0f, 0.0f, 1.0f); GLError . checkThrow ()
+        glClearColor (0.0f, 0.0f, 0.0f, 1.0f); GLError . checkThrow ()
 
         glDisable(GL_CULL_FACE); GLError.checkThrow()
         //glFrontFace(GL_CCW); GLError.checkThrow()
@@ -34,39 +33,36 @@ class Scene(private val window: GameWindow) {
         glEnable(GL_DEPTH_TEST); GLError.checkThrow()
         glDepthFunc(GL_LESS); GLError.checkThrow()
 
-        //1.2.1
-        // Vertices (position) und indices definiert mit float und int Array
+        //1.2.1 Vertices (position) und indices definiert mit float und int Array
         val vertexDataInitialien = floatArrayOf(
-        0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-        0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-        0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f
-        )
+            0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
+            0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f)
 
         val vertexDataHaus = floatArrayOf(
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-        0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f)
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f)
 
-        var vertexData = floatArrayOf(
-                1.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-                0.0f,  1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-                -1.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-                0.0f,  -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f)
+       /* Wird nicht benötigt kommt von OBJloader
+        var vertexDataKreis = floatArrayOf(
+            1.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            -1.0f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            0.0f,  -1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f)*/
 
         val iboHaus = intArrayOf(
             0, 1, 2,
             0, 2, 4,
-            4, 2, 3
-        )
+            4, 2, 3)
 
         val iboInitialien = intArrayOf(
             0, 1, 2,
-            2, 3, 4
-        )
+            2, 3, 4)
 
         val vertexpos = VertexAttribute(3, GL_FLOAT, 24, 0)     //Attribut für Position
         val vertexcol = VertexAttribute(3, GL_FLOAT, 24, 12)    //Attribut für Farbe
@@ -75,39 +71,35 @@ class Scene(private val window: GameWindow) {
         meshhaus = Mesh(vertexDataHaus, iboHaus, vertexzusammen)
         meshname = Mesh(vertexDataInitialien, iboInitialien, vertexzusammen)
 
-        //Aufgabe 1.2.5:
-        //glEnable ( GL_CULL_FACE )
-        //glFrontFace ( GL_CCW )
-        //glCullFace ( GL_BACK )
+        data class Vertex(val position: Vector3f, val texCoord: Vector2f, val normal: Vector3f)
+
+        //Aufgabe 1.2.5: Gegen den Uhrzeigersinn def. Dreiecke rendern
+        /*glEnable (GL_CULL_FACE)
+        glFrontFace (GL_CCW)
+        glCullFace (GL_BACK)*/
 
 
         // Aufgabe 1.3 Object Handeling
-        // 1.3.1 a):
-        // Objekt laden und ein Mesh erzeugen
+        // 1.3.1 a) Objekt laden und ein Mesh erzeugen
+        val res: OBJLoader.OBJResult = OBJLoader.loadOBJ("assets/models/sphere.obj", false, false)
 
-        data class Vertex(val position: Vector3f, val texCoord: Vector2f, val normal: Vector3f)
-
-        val vbo : VertexAttribute
-        // 1.3.1 b):
-        //Erstes Mesh vom ersten Objekt erhalten
+        // 1.3.1 b) Erstes Mesh vom ersten Objekt erhalten
         val objMesh: OBJLoader.OBJMesh = res.objects[0].meshes[0]
         val objMeshList: MutableList<OBJLoader.OBJMesh> = res.objects[0].meshes
 
         OBJLoader.reverseWinding(objMesh)
         OBJLoader.recalculateNormals(objMesh)
 
-        // 1.3.3 d):
-        //Mesh erstellen und VertexAttribute definieren, alle 3 Attribute anlegen damit Shader es nutzen kann
-        val attrPos =   VertexAttribute(3, GL_FLOAT, 8*4, 0)        //position
+        // 1.3.3 d) Mesh erstellen und VertexAttribute definieren, alle 3 Attribute anlegen damit Shader diese nutzen kann
+        val attrPos =   VertexAttribute(3, GL_FLOAT, 8*4, 0)       //position
         val attrTC =    VertexAttribute(2, GL_FLOAT, 8*4, 12)      //textureCoordinate
         val attrNorm =  VertexAttribute(3, GL_FLOAT, 8*4, 20)      //normalval
 
         val vertexAttributes = arrayOf<VertexAttribute>(attrPos, attrTC, attrNorm)
 
-        //1.3.1 c):
-        //Vertex- und Indexdaten als Arrays definieren
-        vertexData = objMesh.vertexData //get vertexdata
-        //indexData = objMesh.indexData   //get indexddata
+        //1.3.1 c): Vertex- und Indexdaten als Arrays definieren
+        var vertexDataKreis = objMesh.vertexData //get vertexdata
+        var indexDataKreis = objMesh.indexData   //get indexddata
 
         //1.3.1 d)
         // use plain data arrays to create a mesh
@@ -115,7 +107,6 @@ class Scene(private val window: GameWindow) {
 
     }
 
-    //1.2.3
     fun render(dt: Float, t: Float) {
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
@@ -123,7 +114,7 @@ class Scene(private val window: GameWindow) {
         //welcher Shader = staticShader
         staticShader.use()
 
-        meshhaus.render()
+        //meshhaus.render()
         //meshname.render()
 
         //1.3.1 e)
